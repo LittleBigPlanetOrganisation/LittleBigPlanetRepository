187952548a07a7024b35cb1a9efa60a7bf5b9c8b
Krystel <Krystelmenot@gmail.com>
2020-11-24 19:34:15 +0100

    Add dbServer, nuGet, config: DbClientOption, appsetting.Dev, Startup

--- src/UserAccount.Api/Startup.cs
+++ src/UserAccount.Api/Startup.cs
@@ -2,33 +2,85 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserAccount.Infrastructure;
 
 namespace UserAccount.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _environment;
+        // private const int CacheMemorySize = 350000000;
+
+        public Startup(IHostEnvironment environment, IConfiguration configuration)
         {
-            Configuration = configuration;
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddHealthChecks()
+                .AddCheck("Default", () => HealthCheckResult.Healthy("OK"))
+            ;
+
+            var options = Options.Create(new DbClientOptions
+            {
+                LittleBigPlanetData = () => new SqlConnection(_configuration.GetConnectionString("LittlebigPlanetData")),
+            });
+            services.AddTransient(x => options);
+
+            //ProviderConfigurationDictionary configurations = new ProviderConfigurationDictionary();
+            //configurations.ProviderConfigurations = GetProvidersConfig();
+            //services.AddTransient(x => configurations);
+
+            services
+                // .AddCustomSwaggerGen<Startup>()
+                // .AddDefaultHttpClient()
+                .AddResponseCompression()
+                .AddDataProtection();
+
+            services
+                .AddMvcCore()
+                .AddDataAnnotations()
+                .AddAuthorization()
+                .AddApiExplorer()
+                //.AddUnifiedRestApi()
+                .AddCors();
+
+            // Register Domain handler
+            /* services     
+                  .AddSingleton<IUserProvider, UserProvider>();
+                  .AddSingleton<ISupervisorRepository, SupervisorRepository>(); */
         }
 
+        // //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddControllers();
+        //}
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+
+        // ////////////////////////////////////////////////ytyryryrtyrtyrt
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ src/UserAccount.Infrastructure/DbClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace UserAccount.Infrastructure
+{
+    public class DbClientOptions : IOptions<DbClientOptions>
+    {
+        public DbClientOptions Value => this;
+
+
+        public Func<IDbConnection> LittleBigPlanetData { get; set; }
+
+    }
+}
