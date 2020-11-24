d1493ee3daf0e7b2fd6b16a8b188be0283e7cf74
Krystel <Krystelmenot@gmail.com>
2020-11-24 23:27:01 +0100

    add repository and interfacce

---
+++ src/UserAccount.Infrastructure/Repository/IUserAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAccount.Infrastructure.Repository
+{
+    public interface IUserAccountRepository
+    {
+    }
+}
---
+++ src/UserAccount.Infrastructure/Repository/UserAccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAccount.Infrastructure.Repository
+{
+    public class UserAccountRepository : IUserAccountRepository
+    {
+        /// <summary>
+        /// The connection
+        /// </summary>
+        private readonly Func<IDbConnection> _connection;
+
+        /// <summary>
+        /// The cache
+        /// </summary>
+        private readonly IMemoryCache _cache;
+
+        /// <summary>
+        /// The cache configuration
+        /// </summary>
+        //private readonly CacheConfiguration _configuration;
+
+
+        public UserAccountRepository(IOptions<DbClientOptions> connection, IMemoryCache cache /*CacheConfiguration configuration*/)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+            /* _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));*/
+            _connection = connection?.Value?.LittleBigPlanetData ?? throw new ArgumentNullException(nameof(connection));
+        }
+    }
