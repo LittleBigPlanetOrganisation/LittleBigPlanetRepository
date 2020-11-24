6d16c41ff653df1033f8ce5db5e62501611e5d63
Krystel <Krystelmenot@gmail.com>
2020-11-24 23:06:08 +0100

    add constant/test

---
+++ src/UserAccount.Infrastructure/Constant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAccount.Infrastructure
+{
+    public static class Constant
+    {
+        public static class StoredProcedure
+        {
+            public static class UserAccount
+            {
+                /// <summary>
+                /// 
+                /// </summary>
+                public static readonly string CreateUserAccount = "createUserAccount";
+                /// <summary>
+                /// 
+                /// </summary>
+                public static readonly string GetUserAccount = "createUserAccount";
+                /// <summary>
+                /// 
+                /// </summary>
+                public static readonly string SetUserAccount = "createUserAccount";
+                /// <summary>
+                /// 
+                /// </summary>
+                public static readonly string DeleteUserAccount = "createUserAccount";
+            }
+
+        }
+    }
+}
