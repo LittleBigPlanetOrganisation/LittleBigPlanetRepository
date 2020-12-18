2cb288f062bc2a35db029430a1a3c9f0a45519f4
Krystel <Krystelmenot@gmail.com>
2020-12-18 16:51:50 +0100

    _Model, _ViewModel

---
+++ src/UserAccount.Api/ViewModels/UserAccountViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAccount.Api.ViewModels
+{
+    public class UserAccountViewModel
+    {
+        public long IdUser { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SurName { get; set; }
+        public string PostalAddress { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public string UrlPicture { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
